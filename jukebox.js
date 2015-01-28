a8cfd6afc245ff9b23c975e00b9c7fb69329cb4a
wxu <winnielandau@gmail.com>
2015-01-27 17:23:18 -0800

    create list item for song submitted, clear the text field.

--- jukebox.js
+++ jukebox.js
@@ -1,1 +1,12 @@
-// Your code goes here.
+$(document).ready( function() {
+
+//$('h1').text("CHANGE");
+
+	$('input[type="submit"]').on('click', function() {
+		var song_name = $('input[type="text"]').val();
+		$('#song-queue').append('<li>'+song_name+'</li>');
+		event.preventDefault();
+		$('input[type="text"').val('');
+	});
+
+});
