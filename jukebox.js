1a185fd2bc173a7bb47e0f3fd5f6db9ca03e201d
wxu <winnielandau@gmail.com>
2015-01-27 18:31:50 -0800

    Play button functionality: play song, remove from list and play next until empty queue

--- jukebox.js
+++ jukebox.js
@@ -1,5 +1,4 @@
 $(document).ready( function() {
-
 //$('h1').text("CHANGE");
 
 	$('input[type="submit"]').on('click', function() {
@@ -9,4 +8,32 @@ $(document).ready( function() {
 		$('input[type="text"').val('');
 	});
 
+	$('#play-button').on('click', playFirstSong );
+
 });
+
+var changeDisplay = function() {
+	$('li:first-child').remove(); //remove the song from list queue
+//	$('#play-button').slideDown();
+	$('#play-button').attr('disabled', false);
+	$('#play-button').html("Play");
+};
+
+var playFirstSong = function() {
+//	$('#play-button').slideUp();
+	var song = $('ul li:first-child').text();
+	if ( song != "" ) {
+		//change the PLAY button appearance
+		$('#play-button').attr('disabled', true);
+		$('#play-button').html("Playing...");
+//		playSong(parseSong(song), 400, changeDisplay );
+		playSong(parseSong(song), 400, playFirstSong );
+		changeDisplay();
+	};
+};
+
+/*
+C*6 B*3 A*5 D*4
+A C*5 D*10 B*6
+A A A 
+ */
