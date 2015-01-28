064b56aae07dd15e0eeaf3c255faa67cad3bc2e7
wxu <winnielandau@gmail.com>
2015-01-28 09:13:16 -0800

    add spacebar support

--- jukebox.js
+++ jukebox.js
@@ -20,13 +20,19 @@ $('h1').text("CHANGE");
 
 		//fadeIn/Out song_music on mouse moves
 		$(myLi).on('mouseenter', function() {
-			$(myLi).children().last().show();
+			$(myLi).children().last().fadeIn();
 		});
 		$(myLi).on('mouseleave', function() {
-			$(myLi).children().last().hide();
+			$(myLi).children().last().fadeOut();
 		});
 	});
 
+	$(document).on('keypress', function(event) {
+	  if (event.which == 32) { //spacebar is 32
+	    //String.fromCharCode(32);
+	    playFirstSong();
+	  };
+	});
 
 	$('#play-button').on('click', playFirstSong );
 
@@ -41,9 +47,11 @@ var changeDisplay = function() {
 
 var playFirstSong = function() {
 	var tmpLi = $('ul li:first-child');
+
 	if ( tmpLi.text() != "" ) {
 		var sg_name = tmpLi.children().first().text();
 		var sg_music = tmpLi.children().last().text();
+
 		//change the user-msg & PLAY button appearance
 		$('#user-msg').html("Now playing: "+sg_name);
 		$('#play-button').attr('disabled', true);
@@ -62,4 +70,7 @@ var playFirstSong = function() {
 C*6 B*3 A*5 D*4
 A C*5 D*10 B*6
 A A A 
- */
+
+F#*1 F#*1 D*1 B*3 B*1 E*2 E*1 E*1 G#*1 G#*1 A*1 B*1 A*1 A*1 A*1 E*1 E*1 F#*2 F#*1 E*1 E*1 F#*1 E*1 D*1 D*1 C#*1 B*1 C#*1 C#*1
+
+A*1 D*1 A*1 D*1 A*1 C*1 A*1 C*1 C*1 E*3 E*1 D*1 E*1 F*1 D*3 A*1 D*1 A*1 D*1 A*1 C*1 A*1 C*1 C*1 E*3 E*1 D*1 E*1 F*1 D*3 D*1 D*1 D*1 C*1 A*1 D*1 D*1 D*1 C*1 A*1 C*1 C*1 C*1 A#*1 A*1 F*1 */
